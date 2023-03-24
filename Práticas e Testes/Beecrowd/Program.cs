ad5ae57e6df13cc40b861288f87acd91ede03a25
levisonjr <levisonjr21@gmail.com>
2023-03-24 15:47:16 -0300

    Arquivos iniciais de exercicios feitos

---
+++ Práticas e Testes/Beecrowd/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Program
+{   
+    class Mat
+    {
+        public static int Soma(params int[] numeros)
+        {
+            int somatorio = 0;
+
+            foreach (var item in numeros)
+            {
+                somatorio += item;
+            }
+
+            return somatorio;
+        }
+    }
+    static class AnalisarTriangulo
+    {
+        static string resultadoAnalise;
+
+        public static void Analisar(float[] arr)
+        {
+            Array.Sort(arr);
+            Array.Reverse(arr);
+
+            float A = arr[0], B = arr[1], C = arr[2];
+
+            if (A >= (B + C))
+            {
+                resultadoAnalise = "NAO FORMA TRIANGULO\n";
+            }
+            else
+            {
+                if (Math.Pow(A, 2) == Math.Pow(B, 2) + Math.Pow(C, 2))
+                {
+                    resultadoAnalise = "TRIANGULO RETANGULO\n";
+                }
+
+                else if (Math.Pow(A, 2) > Math.Pow(B, 2) + Math.Pow(C, 2))
+                {
+                    resultadoAnalise = "TRIANGULO OBTUSANGULO\n";
+                }
+
+                else if (Math.Pow(A, 2) < Math.Pow(B, 2) + Math.Pow(C, 2))
+                {
+                    resultadoAnalise = "TRIANGULO ACUTANGULO\n";
+                }
+
+                if (A == B && B == C)
+                {
+                    resultadoAnalise += "TRIANGULO EQUILATERO\n";
+                }
+                else if ((A == B) || (B == C) || (C == A))
+                {
+                    resultadoAnalise += "TRIANGULO ISOSCELES\n";
+                }
+            }
+
+            Console.Write(resultadoAnalise);
+        }
+    }
+    class Beecrowd
+    {
+        static void Main(string[] args)
+        {
+            string teste = "UUAUUC";
+            Console.WriteLine(teste.Length);
+        }
+    }
+}
---
+++ Práticas e Testes/Exercism/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism
+{
+    public static class ReverseString
+    {
+        public static string Reverse(string input)
+        {
+            string reverse = "";
+            for (int i = input.Length - 1; i <= 0; i--)
+            {
+                Console.WriteLine(i);
+                reverse += input[i];
+            }
+            return reverse;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string teste = "cool";
+            Console.WriteLine(ReverseString.Reverse(teste));
+        }
+    }
+}
