27af1129d094ff6a54697c2f53f8422a130b5675
levisonjr <levisonjr21@gmail.com>
2023-03-30 19:05:03 -0300

    Model > ContatoModel adicionado

---
+++ MeuWepApp/Models/ContatoModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MeuWepApp.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+
+        public ContatoModel()
+        {
+            Name = string.Empty;
+            Email = string.Empty;
+            Celular = string.Empty;
+        }
+    }
+}
