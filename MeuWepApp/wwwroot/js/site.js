fed08255188aa93e25a295795d9b923f63db93cd
levisonjr <levisonjr21@gmail.com>
2023-04-16 20:09:27 -0300

    Implementação do jQuery DataTable

--- MeuWepApp/wwwroot/js/site.js
+++ MeuWepApp/wwwroot/js/site.js
@@ -2,3 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $('#tabelaContatos').DataTable({
+        responsive: true,
+        columnDefs: [
+            { responsivePriority: 1, targets: 0 },
+            { responsivePriority: 2, targets: -1 }
+        ],
+        ordering: true,
+        paging: true,
+        searching: true,
+        oLanguage: {
+            sEmptyTable: "Nenhum registro encontrado na tabela",
+            sInfo: "Exibindo de _START_ até _END_ de _TOTAL_ registros",
+            sInfoEmpty: "Mostrar 0 até 0 de 0 Registros",
+            sInfoFiltered: "(Filtrar de _MAX_ total registros)",
+            sInfoPostFix: "",
+            sInfoThousands: ".",
+            sLengthMenu: "Qnt. de contatos por página: _MENU_",
+            sLoadingRecords: "Carregando...",
+            sProcessing: "Processando...",
+            sZeroRecords: "Nenhum registro encontrado",
+            sSearch: "Pesquisar: ",
+            oPaginate: {
+                sNext: "Próximo",
+                sPrevious: "Anterior",
+                sFirst: "Primeiro",
+                sLast: "Último"
+            },
+            oAria: {
+                sSortAscending: ": Ordenar colunas de forma ascendente",
+                sSortDescending: ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
+});
