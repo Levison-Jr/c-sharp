15acc15b41ddcfd3b923a7a1bba350032aae9425
levisonjr <levisonjr21@gmail.com>
2023-04-17 21:56:43 -0300

    Adição da Controller e View de Usuários (Página e navegação básica)

---
+++ MeuWepApp/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeuWepApp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
