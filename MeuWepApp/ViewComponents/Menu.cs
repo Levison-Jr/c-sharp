7a34c7224a643fc917b952d6004743acae6b406e
levisonjr <levisonjr21@gmail.com>
2023-05-25 08:56:25 -0300

    Criação do componente <Menu> para apresentação do nome do usuário logado

---
+++ MeuWepApp/ViewComponents/Menu.cs
@@ -0,0 +1,23 @@
+﻿using MeuWepApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MeuWepApp.ViewComponents
+{
+    public class Menu : ViewComponent
+    {
+        public IViewComponentResult? Invoke()
+        {
+            string? sessaoUsuario = HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if (string.IsNullOrEmpty(sessaoUsuario))
+            {
+                return null;
+            }
+
+            UsuarioModel? usuario = JsonConvert.DeserializeObject<UsuarioModel>(sessaoUsuario);
+
+            return View(usuario);
+        }
+    }
+}
