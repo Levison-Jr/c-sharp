c22265b577b06630cfaccd1f1aaa33c8e02fd927
levisonjr <levisonjr21@gmail.com>
2024-01-19 22:57:54 -0300

    Ajustes simples de execução para os testes

--- OOP_Bank_Example/Program.cs
+++ OOP_Bank_Example/Program.cs
@@ -6,7 +6,7 @@
         {
             try
             {
-                BankAccount customer_1 = new("John Doe", 200);
+                InterestEarningAccount customer_1 = new("John Doe", 200);
                 Console.WriteLine(customer_1.Balance);
 
                 customer_1.MakeDeposit(200, DateTime.Now, "Reservas");
@@ -14,6 +14,8 @@
 
                 customer_1.MakeWithdrawal(150m, DateTime.Now, "Contas");
                 Console.WriteLine(customer_1.Balance);
+
+                customer_1.EndMonthTask();
 
                 Console.WriteLine(customer_1.GetAccountHistory());
             }
