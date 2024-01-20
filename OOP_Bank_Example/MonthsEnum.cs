b2a9920ff6c11958d8cb4206b0e36397b111d7d0
levisonjr <levisonjr21@gmail.com>
2024-01-19 22:55:49 -0300

    Enum conversao de int para nome do mês correspondente

---
+++ OOP_Bank_Example/MonthsEnum.cs
@@ -0,0 +1,18 @@
+﻿namespace OOP_Bank_Example
+{
+    public enum MonthsConvert
+    {
+        Janeiro = 1,
+        Fevereiro,
+        Março,
+        Abril,
+        Maio,
+        Junho,
+        Julho,
+        Agosto,
+        Setembro,
+        Outrubro,
+        Novembro,
+        Dezembro
+    }
+}
