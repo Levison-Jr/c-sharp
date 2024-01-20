93d42e117b2f2d75f86d3c4a38e86b1fad22191a
levisonjr <levisonjr21@gmail.com>
2024-01-19 22:55:02 -0300

    Add tipos de contas bancarias

---
+++ OOP_Bank_Example/GiftCardAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Bank_Example
+{
+    public class GiftCardAccount : BankAccount
+    {
+        public GiftCardAccount(string name, decimal initialBalance) : base(name, initialBalance)
+        {
+            
+        }
+    }
+}
---
+++ OOP_Bank_Example/InterestEarningAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Bank_Example
+{
+    public class InterestEarningAccount : BankAccount
+    {
+        public InterestEarningAccount(string name, decimal initialBalance) : base(name, initialBalance)
+        {
+            
+        }
+    }
+}
