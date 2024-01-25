fd1df96db134172324e1bb76909209f60a2d7513
levisonjr <levisonjr21@gmail.com>
2024-01-24 23:11:27 -0300

    Commit inicial do projeto

---
+++ OOP_Sistema_Estacionamento/SistemaConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using SistemaEstacionamentoLibrary;
+
+namespace SistemaConsoleUI;
+
+public class ConsoleUI
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, Console!");
+        Class1.HelloFromClassLib();
+    }
+}
---
+++ OOP_Sistema_Estacionamento/SistemaEstacionamentoLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaEstacionamentoLibrary;
+
+public class Class1
+{
+    public static void HelloFromClassLib()
+    {
+        Console.WriteLine("Hello, ClassLib!");
+    }
+}
